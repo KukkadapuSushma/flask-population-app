3380f1615a753449360ecc7e790f26918cf22ea4
KukkadapuSushma <14wh1a0582@bvrithyderabad.edu.in>
2019-03-06 12:10:55 -0600

    new update11

--- app.py
+++ app.py
@@ -2,28 +2,34 @@ from flask import Flask, render_template, request, flash
 from time import time
 import pyodbc
 import redis
+from sqlalchemy import create_engine
+import urllib
 import csv
 app = Flask(__name__)
 app.secret_key = "Secret"
 
-r = redis.StrictRedis(host='sushma.redis.cache.windows.net', port=6380, db=0, password='fQrhWzt3pQ5QnCBWDzM6GhSQCBCi8p33qLGVexTPn8I=', ssl=True)
+#r = redis.StrictRedis(host='sushma.redis.cache.windows.net', port=6380, db=0, password='fQrhWzt3pQ5QnCBWDzM6GhSQCBCi8p33qLGVexTPn8I=', ssl=True)
 
-connection = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:sushmak.database.windows.net,1433;Database=quakes;Uid=sushma@sushmak;Pwd={azure@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
-cursor = connection.cursor()
+params = urllib.parse.quote_plus("Driver={ODBC Driver 13 for SQL Server};Server=tcp:sushmak.database.windows.net,1433;Database=quakes;Uid=sushma@sushmak;Pwd={azure@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
 
+#connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:sushmak.database.windows.net,1433;Database=quakes;Uid=sushma@sushmak;Pwd={azure@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+#cursor = connection.cursor()
 '''
 @app.route('/')
 def index():
     start_time = time()
-    cursor.execute("CREATE TABLE [dbo].[codes](\
+    cursor.execute("CREATE TABLE [dbo].[education](\
+        [entity] [nvarchar](40) NULL,\
         [code] [nvarchar](8) NULL,\
-        [state] [nvarchar](20) NULL)")
+        [yr] int NULL,\
+        [blpercent] float NULL)")
     connection.commit()
 
-    query = "INSERT INTO dbo.codes(code,state) VALUES (?,?)"
+    query = "INSERT INTO dbo.education(entity,code,yr,blpercent) VALUES (?,?,?,?)"
     
 
-    with open('statecode.csv') as csvfile:
+    with open('educationshare.csv') as csvfile:
         next(csvfile)
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
@@ -34,14 +40,29 @@ def index():
         time_taken = (end_time - start_time)
         flash('The Average Time taken to execute the random queries is : ' + "%.4f" % time_taken + " seconds")
         cursor.close()
-    return render_template('index.html', t=time_taken)'''
-
+    return render_template('index.html', t=time_taken)
+'''
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/barchart', methods=['POST'])
+def bar():
+    qot = '\''
+    if request.form['form'] == 'ShowGraph':
+        mag1 = request.form['vmag1']
+        mag2 = request.form['vmag2']
+        rows = engine.execute("select CAST(locationsource as varchar(max)) as locationsource,count(*) as cnt from quake where mag between "+mag1+" and "+mag2+" group by CAST(locationsource as varchar(max));").fetchall()
+        #rows = engine.execute("select top(1000) id, latitude, longitude, mag, CAST(locationsource as varchar(max)) as locationsource from earthquake2;").fetchall()
+        rows = [dict(row) for row in rows]
+        #return render_template('visual.html', a=rows, chartType='Scatter')
+        #return render_template('visual.html', a=rows, chartType='Pie')
+        return render_template('display.html', a=rows, chartType='Bar')
 
+
+
+'''
 @app.route('/server', methods=['GET', 'POST'])
 def server():
     tag1 = request.form['tag1']
@@ -76,7 +97,6 @@ def server():
     t = e-s
     return render_template('magGreater.html', t=str(t), re=r)
 
-
 @app.route('/qstn8', methods=['GET', 'POST'])
 def qstn8():
     tag1 = request.form['tag8']
@@ -90,7 +110,6 @@ def qstn8():
     #e = time()
     #t = e-s
     #return render_template('magGreater.html', data=r, t=t)
-
 
 @app.route('/qstn9', methods=['GET', 'POST'])
 def qstn9():
@@ -153,3 +172,4 @@ def serverCache():
     e = time()
     t = e - s
     return render_template('magGreater.html', t1=t, re1=data)
+'''
